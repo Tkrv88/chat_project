39d2ae7000f5967d7c67ac994c85ca9b568a345d
Токарев Алексей <alex-tok-8alex@mail.ru>
2023-03-09 00:14:08 +0700

    Added models

--- chat/main/models.py
+++ chat/main/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Chat(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=100)
+    privacy = models.BooleanField(default=True)
+    create_date = models.DateField(
+        'Дата создания',
+        auto_now_add=True
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=False,
+    )
+    chat = models.ForeignKey(
+        Chat,
+        on_delete=models.CASCADE,
+        blank=False
+    )
+    text = models.TextField()
+    send_date = models.DateTimeField(auto_now_add=True)
