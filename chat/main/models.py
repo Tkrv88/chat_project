b39fa5269ef88787085e04f7847c6fc0957c2e87
Max <www.masyanya89.ru@gmail.com>
2023-03-09 17:11:54 +0300

    create model of message and chat

--- chat/main/models.py
+++ chat/main/models.py
@@ -1,9 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
 
+user = get_user_model()
+
+class Chat(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    create_date = models.DateField()
+
 class Message(models.Model):
-    time = models.DateTimeField(auto_now_add=True)
-    text = models.TextField()
-    user_id = models.IntegerField()
-    message_id = id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE(),
+        blank=False,
+    )
+    chat = models.ForeignKey(
+        Chat,
+        on_delete=models.CASCADE(),
+        blank=False
+    )
+
 # Create your models here.
